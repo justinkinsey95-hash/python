dea7cc2fabbe1df355141588f3261080de681c94
Justin_Kinsey <justinkinsey95@gmail.com>
2025-11-22 23:42:32 -0600

    changed doc strings

--- television.py
+++ television.py
@@ -8,7 +8,7 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self):
+    def __init__(self) -> None:
         """
         sets up the television as off, not muted, volume 0, and channel 0
         """
@@ -28,7 +28,6 @@ class Television:
         """
         Turns the television on when off
         and off when on
-        :return: returns True if on and False if off
         """
         if self.__status:
             self.__status = False
@@ -38,7 +37,6 @@ class Television:
     def mute(self) -> None:
         """
         Mutes and unmutes the television when turned on
-        :return: returns True if unmuted or False if muted
         """
         if self.__status:
             if self.__muted == False:
@@ -49,7 +47,6 @@ class Television:
     def channel_up(self) -> None:
         """
         Turns the channel up to the max and then back to the min
-        :return: returns the new channel number as an int
         """
         if self.__status:
             if self.__channel == Television.MAX_CHANNEL:
@@ -60,7 +57,6 @@ class Television:
     def channel_down(self) -> None:
         """
         Turns the channel down to the min and then back to the max
-        :return: returns the new channel number as an int
         """
         if self.__status:
             if self.__channel == Television.MIN_CHANNEL:
@@ -71,7 +67,6 @@ class Television:
     def volume_up(self) -> None:
         """
         Turns the volume up one int at a time up until the max volume
-        :return: returns the new volume as an int
         """
         if self.__status:
             self.__muted = False
@@ -83,7 +78,6 @@ class Television:
     def volume_down(self) -> None:
         """
         Turns the volume down one int at a time down until the min volume
-        :return: returns the new volume as an int
         """
         if self.__status:
             self.__muted = False
